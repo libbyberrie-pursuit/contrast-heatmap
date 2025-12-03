a43af5966b86a512ffdcefb7d26baebb5436d028
Libby Berrie <libby.berrie@acorn.works>
2025-12-03 16:17:22 +1100

    Highlight low contrast areas

--- generate-heatmap.js
+++ generate-heatmap.js
@@ -1,14 +1,6 @@
-const { intToRGBA, rgbaToInt, colorDiff } = require("@jimp/utils");
+const { intToRGBA, rgbaToInt } = require("@jimp/utils");
 const { Jimp } = require('jimp');
-const {fs} = require('fs');
 
-
-// function getRandomRgbaInt(){
-//   let r = Math.floor(Math.random() * 256); // Random between 0-255
-//   let g = Math.floor(Math.random() * 256); // Random between 0-255
-//   let b = Math.floor(Math.random() * 256); // Random between 0-255
-//   return rgbaToInt(r, g, b, 255);
-// };
 
 
 //----------- Color Contrast Calculations ----------
@@ -31,7 +23,7 @@ function srgbToLinear(c) {
  * Uses WCAG's luminance formula, which weights R, G, B differently
  * based on human visual sensitivity.
  */
-function luminance({ r, g, b, a}) {
+function luminance({ r, g, b}) {
   const R = srgbToLinear(r);
   const G = srgbToLinear(g);
   const B = srgbToLinear(b);
@@ -61,7 +53,25 @@ function contrastRatio(color1, color2) {
 
 
 async function generateHeatmap() {
-  const inputImage = await Jimp.read('input/test-screenshot-small.png')
+  const originalImage = await Jimp.read('input/test-screenshot-small.png');
+  //we want to sharpen the image to make sure we're not reading any browser anti-aliasing. 
+  //references: 
+  // https://en.wikipedia.org/wiki/Kernel_(image_processing)
+  // https://jimp-dev.github.io/jimp/api/jimp/classes/jimp/#convolute
+  // https://blog.demofox.org/2022/02/26/image-sharpening-convolution-kernels/
+  // const inputImage = originalImage.clone().convolution([
+  //   [ 0, -1,  0],
+  //   [-1,  5, -1],
+  //   [ 0, -1,  0]
+  // ]);
+
+  const inputImage = originalImage.clone().convolution([
+    [ 0.0023, -0.0432,  0.0023],
+    [-0.0432,  1.182, -0.0432],
+    [ 0.0023, -0.0432,  0.0023]
+  ]);
+  inputImage.write('output/temp-sharper.png');
+  // const inputImage = originalImage.clone().pixelate(6);
   const outputImage = new Jimp({ width: inputImage.bitmap.width, height: inputImage.bitmap.height, color: 0xffffff00 });
 
   const maxWidth = inputImage.bitmap.width;
@@ -69,7 +79,9 @@ async function generateHeatmap() {
 
   // METHOD ONE: using while loop with manual coordinate handling
   const currentCoordinates = { x: 0, y: 0 };
-  const alertColor = (differentSiblingsCount) => rgbaToInt(234, 14, 14, ((255*0.125)*differentSiblingsCount))
+  // -3 to +3 step for siblings (49 possible siblings)
+  // const alertColor = (differentSiblingsCount) => rgbaToInt(234, 14, 14, ((255*0.0052)*differentSiblingsCount))
+  const alertColor = (count, multiplier) => rgbaToInt(234, 14, 14, Math.min(255, Math.round(255 * multiplier * count)));
 
   function withinBounds(coord) {
     return (coord.x >= 0 && coord.x < maxWidth) && (coord.y >= 0 && coord.y < maxHeight)
@@ -80,16 +92,20 @@ async function generateHeatmap() {
     const currentColor = intToRGBA(inputImage.getPixelColor(currentCoordinates.x, currentCoordinates.y));
 
     //step 1: get array of all pixel 'siblings' (adjacent pixels)
-    const siblings = [-1, 0, 1].flatMap(dx =>
-      [-1, 0, 1].map(dy => ({ x: currentCoordinates.x + dx, y: currentCoordinates.y + dy }))
+    const siblings = [-3, -2, -1, 0, 1, 2, 3].flatMap(dx =>
+      [-3, -2, -1, 0, 1, 2, 3].map(dy => ({ x: currentCoordinates.x + dx, y: currentCoordinates.y + dy }))
     ).filter(coord => withinBounds(coord) && !(coord.x === currentCoordinates.x && coord.y === currentCoordinates.y))
      .filter(sibling => {
       const siblingColor = intToRGBA(inputImage.getPixelColor(sibling.x, sibling.y));
       const siblingRatio = contrastRatio(currentColor, siblingColor);
-      return (siblingRatio >= 3);
+      // console.dir(siblingRatio);
+      return ((siblingRatio !== 1) && (siblingRatio <= 3));
     })
- 
-    outputImage.setPixelColor(alertColor(siblings.length), currentCoordinates.x, currentCoordinates.y);
+    // console.log('total siblings with sufficient contrast:', siblings.length);
+    if (siblings.length > 0) {
+      const opacityMultiplier = siblings.length > 25 ? 0.02 : 0.0052;
+      outputImage.setPixelColor(alertColor(siblings.length, opacityMultiplier), currentCoordinates.x, currentCoordinates.y);
+    }
         
 
     if (currentCoordinates.x < maxWidth - 1) {
